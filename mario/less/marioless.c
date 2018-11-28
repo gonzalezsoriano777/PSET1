81e35cdaa0d74029eb3efa2d0efa19f35510febe
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-11-28 18:13:29 +0000

    Runs good, success

--- mario/less/marioless.c
+++ mario/less/marioless.c
@@ -1,3 +1,4 @@
+// A Mario pyramid
 #include <cs50.h>
 #include <stdio.h>
 
@@ -13,24 +14,24 @@ int main(void)
         heights = get_int();
     }
     // the formula for the height
-   while(heights < 0 || heights > 23);
+    while (heights < 0 || heights > 23);
 
 // formula for line, where line is less than heights and if its less then height, you keep adding lines
- for (int line = 0; line < heights; line++)
-   {
-       // formula for spaces, depending on the int for heights it will be subtracted with the line which is always 0, if it is greater then one it subtracts the spaces
-     for (int spaces = heights - line; spaces > 1; spaces--)
-     {
-         printf(" ");
-     }
-     // formula for hashes
-     for (int hashes = 0; hashes < line + 2; hashes++)
-     {
-        printf("#");
-     }
-     // to make new lines for each line of hashes
-     printf("\n");
-   }
+    for (int line = 0; line < heights; line++)
+    {
+        // formula for spaces, depending on the int for heights it will be subtracted with the line which is always 0, if it is greater then one it subtracts the spaces
+        for (int spaces = heights - line; spaces > 1; spaces--)
+        {
+            printf(" ");
+        }
+        // formula for hashes
+        for (int hashes = 0; hashes < line + 2; hashes++)
+        {
+            printf("#");
+        }
+        // to make new lines for each line of hashes
+        printf("\n");
+    }
 
 }
 
